15a83cf24e1a67c20ba7c09e7003ba6e5543c29e
noot <elizabethjbinks@gmail.com>
2022-10-09 13:02:30 -0400

    add homomorphism test and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# go-paillier
+
+This library is an implementation of the [Paillier cryptosystem](https://en.wikipedia.org/wiki/Paillier_cryptosystem) which is an asymmetric encryption algorithm. It has homomorphic properties; ie. multiplying two ciphertexts decrypts to the sum of their plaintexts. This property is leveraged for threshold ECDSA.
+
+I wrote this for learning purposes only :)
--- paillier_test.go
+++ paillier_test.go
@@ -2,6 +2,7 @@ package paillier
 
 import (
 	"crypto/rand"
+	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -19,4 +20,30 @@ func TestEncryption(t *testing.T) {
 	res := kp.Decrypt(c)
 	require.NoError(t, err)
 	require.Equal(t, m, res)
+}
+
+func TestEncryptionHomomorphic(t *testing.T) {
+	kp, err := GeneratePrivateKey(1024)
+	require.NoError(t, err)
+
+	m1, err := rand.Int(rand.Reader, kp.N)
+	require.NoError(t, err)
+	c1, err := kp.Encrypt(m1)
+	require.NoError(t, err)
+
+	m2, err := rand.Int(rand.Reader, kp.N)
+	require.NoError(t, err)
+	c2, err := kp.Encrypt(m2)
+	require.NoError(t, err)
+
+	n2 := (&big.Int{}).Mul(kp.N, kp.N)
+	cProd := (&big.Int{}).Mul(c1.c, c2.c)
+	cProd = (&big.Int{}).Mod(cProd, n2)
+
+	mSum := (&big.Int{}).Add(m1, m2)
+	mSum = (&big.Int{}).Mod(mSum, kp.N)
+
+	res := kp.Decrypt(&Ciphertext{cProd})
+	require.NoError(t, err)
+	require.Equal(t, mSum, res)
 }
